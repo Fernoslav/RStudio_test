56339622201340262e146153c3fb443b97990d56
Hubert Kolano <hubertkolano00@gmail.com>
2024-06-25 20:36:59 +0200

    Changed fuelage to normal units

--- test_wykres.R
+++ test_wykres.R
@@ -4,8 +4,9 @@ library(ggplot2)
 
 data(mtcars)
 
+mtcars$l_per_100km <- 235.214583 / mtcars$mpg
 
-ggplot(data = mtcars, aes(x = factor(cyl), y = mpg)) +
+ggplot(data = mtcars, aes(x = factor(cyl), y = l_per_100km)) +
   stat_summary(fun = mean, geom = "bar", fill = "skyblue", color = "black") +
   labs(
     title = "Średnie zużycie paliwa dla różnych liczby cylindrów",
